b7dd66f1538b272f91742725f8fa9bfa6b6caf8a
Daniel <danne_1998@hotmail.se>
2020-09-08 17:38:40 +0200

    big stuff going on, almost done

--- lab1/gen_hash.cpp
+++ lab1/gen_hash.cpp
@@ -3,11 +3,26 @@
 
 using namespace std;
 
+int get_value(char c){
+    switch(c){
+        case 228:
+            return 26;
+        case 229:
+            return 27;
+        case 246:
+            return 28;
+        default:
+            return c - 'a';
+    }
+}
 u_int32_t gen_hash(string s){
     u_int32_t hash = 0;
-    hash += (s[0] - 'a')*900;
-    hash += (s[1] - 'a')*30;
-    hash += (s[2] - 'a');
+    hash += get_value(s[0])*900;
+    hash += get_value(s[1])*30;
+    hash += get_value(s[2]);
     return hash;
 }
 
+//ä = 228
+//å = 229
+//ö = 246
--- lab1/gen_index.cpp
+++ lab1/gen_index.cpp
@@ -10,23 +10,38 @@
 using namespace std;
 
 map<string, vector<u_int32_t>> i_map;
-map<u_int32_t, u_int32_t> a_map;
+u_int32_t a_map[27000] = {0};
 
-u_int32_t find_index(string s, int startPos){
+u_int32_t find_index(string s, int &pos){
     map<string, vector<u_int32_t>>::iterator it = i_map.begin();
-    //advance(it, startPos);
+    advance(it, pos);
+    int hash = gen_hash(s);
     u_int32_t offset = 0;
+    if(s != "aaa") offset = a_map[gen_hash(s)-1];
     for(it; it != i_map.end(); it++){
-        if(it->first.substr(0,3) == s) break;
-
-        cout << "IT-FIRST : " << it->first << endl;
-        cout << "S : " << s << endl;
-        cout << "SIZEOF : " << sizeof(it->first) + sizeof(it->second) << endl;
-
-        offset += sizeof(it->first) + sizeof(it->second);
+        int it_hash = gen_hash(it->first.substr(0,3));
+        if(it_hash == hash){
+            //cout << "POSITION = " << pos << endl;
+            //cout << "HASH = " << hash << " : IT_HASH = " << it_hash << endl;
+            //cout << "OFFSET = " << offset << endl;
+            break;
+        } 
+        /*if(it_hash > hash){
+            offset = 0;
+            if(s != "aaa") offset = a_map[gen_hash(s)-1];
+            break;
+        }*/
+        //cout << it->first << endl;
+        pos++;
+        offset += it->first.size() + it->second.size()*sizeof(u_int32_t);
+        //cout << "OFFSET++ = " << offset << endl;
     }
-    if(it == i_map.end()) offset = startPos;
-    cout << offset << endl;
+    if(it == i_map.end()){
+        pos = 0;
+        offset = 0;
+        if(s != "aaa") offset = a_map[gen_hash(s)-1];
+    }
+    //cout << offset << endl;
     return offset;
 }
 
@@ -40,39 +55,70 @@ void gen_i(std::istream &in){
     }
 }
 
+/*
 void gen_a(){
-    int prevPos=0;
+    int prevPos = 0;
+    
     for(int i = 0; i < 30; i++){
         for(int j = 0; j < 30; j++){
             for(int k = 0; k < 30; k++){
                 string s = {(char)('a' + i), (char)('a' + j), (char)('a' + k)};
                 u_int32_t hash = gen_hash(s);
-                prevPos = find_index(s, prevPos);
-                a_map[hash] = prevPos;
+                a_map[hash] = find_index(s, prevPos);
+                cout << k + j*30 + i*900 << endl;
             }
         }
     }
+    
 }
+*/
+
+
+void gen_a_2_PUNKT_NOLL(){
+    map<string, vector<u_int32_t>>::iterator it = i_map.begin();
+    u_int32_t offset = 0;
+    for(it; it != i_map.end(); it++){
+        cout << it->first << endl;
+        int hash = 0;
+        if(it->first.length() < 3){
+            string s = {' ', ' ', ' '};
+            for(int i = 0; i < it->first.length(); i++){
+                s[i] = it->first[i];
+            }
+            for(int i = 2; i > it->first.length(); i--){
+                s[i] = 'a';
+            }
+            hash = gen_hash(s);
+        }else hash = gen_hash(it->first.substr(0,3));
+        if(a_map[hash] == 0) a_map[hash] = offset;
+        offset += it->first.size() + it->second.size()*sizeof(u_int32_t);
+    }
+}
+
 
 int gen_index(std::istream &in){
     
 
     gen_i(in);
-    gen_a();
+    gen_a_2_PUNKT_NOLL();
+
+    cout << endl << endl << endl;
+    cout << "------------I FILEN BÖRJAR HÄR------------------" << endl;
+    cout << endl << endl << endl;
 
     /*for(auto elem : i_map){
         cout << elem.first << " : ";
         for(int i = 0; i < elem.second.size(); i++)
             cout << elem.second[i] << " ";
         cout << endl;
-    }*/
-    /*
+    }
+    
     cout << endl << endl << endl;
     cout << "------------A FILEN BÖRJAR HÄR------------------" << endl;
     cout << endl << endl << endl;
-
-    for(auto elem : a_map)
-        cout << elem.first << " : " << elem.second << endl;
+    
+    for(int i = 0; i < 27000; i++)
+        cout << i << " : " << a_map[i] << endl;
     */
     return 0;
 }
