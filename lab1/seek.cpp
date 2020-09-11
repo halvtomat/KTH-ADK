29e6be379fcccc033ee26006a18d213557c0ca50
Daniel <danne_1998@hotmail.se>
2020-09-11 14:51:58 +0200

    bugs found, och, i and ochäldre not working as intended

--- lab1/seek.cpp
+++ lab1/seek.cpp
@@ -11,9 +11,27 @@ const int PRINT_LENGTH = 60;
 
 u_int32_t a[22764] = {0}; //28^3 + 28^2 + 28
 
+void print_konkordans(u_int32_t offset, int word_size, ifstream &korpus){
+    korpus.seekg(offset - PRINT_LENGTH/2 + word_size / 2);
+    string s (PRINT_LENGTH, 'a');
+    korpus.read(&s[0], PRINT_LENGTH);
+    replace(s.begin(), s.end(), '\n', ' ');
+    cout << s << endl;
+}
+
+/**
+ * Function that iterates the I-fil between lower and upper bound in search of 
+ * the given string value. Reads the first 8 bits to know the size of the word and the followed 16 bits
+ * to know the size of the vector. If we find the word we store the byteoffset values in korpus in a vector.
+ * @param u_int32_t, string
+ * @return vector
+ * 
+*/
 vector<u_int32_t> search(u_int32_t lower, u_int32_t upper, string s){
     ifstream i_fil = ifstream("/var/tmp/i_fil.bin", ios::binary);
+    ifstream korpus = ifstream("./korpus");
     i_fil.seekg(lower);
+    int count = 0;
     vector<u_int32_t> v;
     while(i_fil.tellg() < upper){
         u_int8_t word_size = 0;
@@ -25,19 +43,27 @@ vector<u_int32_t> search(u_int32_t lower, u_int32_t upper, string s){
         i_fil.read(&word[0], word.size());
         if(s == word){
             for(int j = 0; j < v_size; j++){
+                count++;
                 u_int32_t elem;
                 i_fil.read(reinterpret_cast<char *>(&elem), sizeof(u_int32_t));
-                v.push_back(elem);
+                print_konkordans(elem, word_size, korpus);
             }
+            cout << "Word Count = "<< count << endl;
             break;
         }
+
         u_int32_t jump = v_size*sizeof(u_int32_t) + i_fil.tellg();
         i_fil.seekg(jump);
     }
+    i_fil.close();
+    korpus.close();
     return v;
 }
 
-
+/**
+ * 
+ * 
+ */
 u_int32_t find_upper(string s){
     u_int32_t upper = gen_hash(s)+1;
     while(a[upper] == 0 && upper < sizeof(a)/sizeof(u_int32_t)){
@@ -68,16 +94,6 @@ void load_a(){
     }
     a_fil.close();
 }
-void print_konkordans(vector<u_int32_t> v, string word){
-    ifstream korpus_maximus = ifstream("./korpus");
-    for(int i = 0; i<v.size();i++){
-        korpus_maximus.seekg(v[i] - PRINT_LENGTH/2 + word.size() / 2);
-        string s (PRINT_LENGTH, 'a');
-        korpus_maximus.read(&s[0], PRINT_LENGTH);
-        replace(s.begin(), s.end(), '\n', ' ');
-        cout << s << endl;
-    }
-}
 
 int main(int argc, char const *argv[]){
     if(argc != 2){
@@ -92,10 +108,7 @@ int main(int argc, char const *argv[]){
     u_int32_t lower = a[find_lower(tmp)];
     u_int32_t upper = a[find_upper(tmp)];
 
-    vector<u_int32_t> v = search(lower, upper, s);
-    if(v.empty()) cout << "Word does not exist" << endl;
-    else{
-        print_konkordans(v, s);
-    }
+    search(lower, upper, s);
+
     return 0;
 }
