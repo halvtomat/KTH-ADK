d744fdd5e5f5f4f1b28a0b03158821ef5ef259bd
Daniel <danne_1998@hotmail.se>
2020-09-21 23:42:13 +0200

    main currently too slow but otherwise working

--- lab2/Main.java
+++ lab2/Main.java
@@ -1,42 +1,89 @@
-/* Labb 2 i DD2350 Algoritmer, datastrukturer och komplexitet    */
-/* Se labbinstruktionerna i kursrummet i Canvas                  */
-/* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-
-  public static List<String> readWordList(BufferedReader input) throws IOException {
-    LinkedList<String> list = new LinkedList<String>();
-    while (true) {
-      String s = input.readLine();
-      if (s.equals("#"))
-        break;
-      list.add(s);
+    public static List<String> readWordList(BufferedReader input) throws IOException {
+        LinkedList<String> list = new LinkedList<String>();
+        while (true) {
+            String s = input.readLine();
+            if (s.equals("#"))
+                break;
+            list.add(s);
+        }
+        return list;
     }
-    return list;
-  }
 
-  public static void main(String args[]) throws IOException {
-    //    long t1 = System.currentTimeMillis();
-    BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-    // Säkrast att specificera att UTF-8 ska användas, för vissa system har annan
-    // standardinställning för teckenkodningen.
-    List<String> wordList = readWordList(stdin);
-    String word;
-    while ((word = stdin.readLine()) != null) {
-      ClosestWords closestWords = new ClosestWords(word, wordList);
-      System.out.print(word + " (" + closestWords.getMinDistance() + ")");
-      for (String w : closestWords.getClosestWords())
-        System.out.print(" " + w);
-      System.out.println();
+    public static void main(String args[]) throws IOException {
+        // double start = System.currentTimeMillis();
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+        List<String> wordList = readWordList(stdin);
+        String word;
+        while ((word = stdin.readLine()) != null) {
+            ClosestWordsOptimized closestWords = new ClosestWordsOptimized(word, wordList);
+            System.out.print(word + " (" + closestWords.getMinDistance() + ")");
+            for (String w : closestWords.getClosestWords())
+                System.out.print(" " + w);
+            System.out.println();
+        }
+        // double totalTime = System.currentTimeMillis() - start;
+        // System.out.println("totaltime = " + totalTime);
     }
-    //    long tottime = (System.currentTimeMillis() - t1);
-    //    System.out.println("CPU time: " + tottime + " ms");
+}
 
-  }
+class ClosestWordsOptimized {
+    LinkedList<String> closestWords = null;
+
+    int closestDistance = -1;
+
+    int min(int a, int b, int c){
+        return Math.min(a, Math.min(b,c));
+    }
+
+    int partDist(String w1, String w2, int w1len, int w2len) {
+        int a[][] = new int[w1len+1][w2len+1];
+        for(int i = 0; i <= w1len; i++)
+            a[i][0] = i;
+        for(int i = 1; i <= w2len; i++)
+            a[0][i] = i;
+        for(int i = 1; i <= w1len; i++){
+            for(int j = 1; j <= w2len; j++){
+                int sub = (w1.charAt(i-1) == w2.charAt(j-1) ? 0 : 1);
+                a[i][j] = min(
+                    a[i-1][j-1] + sub,
+                    a[i][j-1] + 1,
+                    a[i-1][j] + 1
+                    );
+            }
+        }
+        return a[w1len][w2len];
+    }
+
+    int distance(String w1, String w2) {
+        return partDist(w1, w2, w1.length(), w2.length());
+    }
+
+    public ClosestWordsOptimized(String w, List<String> wordList) {
+        for (String s : wordList) {
+            if(Math.abs(w.length() - s.length()) > closestDistance && closestDistance != -1) continue;
+            int dist = distance(w, s);
+            if (dist < closestDistance || closestDistance == -1) {
+                closestDistance = dist;
+                closestWords = new LinkedList<String>();
+                closestWords.add(s);
+            }
+            else if (dist == closestDistance)
+                closestWords.add(s);
+        }
+    }
+
+    int getMinDistance() {
+        return closestDistance;
+    }
+
+    List<String> getClosestWords() {
+        return closestWords;
+    }
 }
--- lab2/partDist_test.java
+++ lab2/partDist_test.java
@@ -1,34 +1,54 @@
+import java.sql.Time;
+
 public class partDist_test {
     static int count = 0;
-    private static int partDist(String w1, String w2, int w1len, int w2len) {
-        count++;
-        if (w1len == 0)
-          return w2len;
-        if (w2len == 0)
-          return w1len;
-        int res = partDist(w1, w2, w1len - 1, w2len - 1) + 
-        (w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
-        int addLetter = partDist(w1, w2, w1len - 1, w2len) + 1;
-        if (addLetter < res)
-          res = addLetter;
-        int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
-        if (deleteLetter < res)
-          res = deleteLetter;
-        return res;
+
+    private static int min(int a, int b, int c){
+        int min = a;
+        if(b < min) min = b;
+        if(c < min) min = c;
+        return min;
     }
-    public static void main(String[] args) {
-        
-        for(int i = Integer.parseInt(args[3]); i >= 0; i--){
-            for(int j = 0; j <= Integer.parseInt(args[2]); j++){
-                int res = partDist(args[0],args[1],j,i);
+
+    private static int partDist(String w1, String w2, int w1len, int w2len) {
+        if(w1len == 0) return w2len;
+        if(w2len == 0) return w1len;
+        int a[][] = new int[w1len+1][w2len+1];
+        for(int i = 0; i <= w1len; i++)
+            a[i][0] = i;
+        for(int i = 1; i <= w2len; i++)
+            a[0][i] = i;
+        for(int i = 1; i <= w1len; i++){
+            for(int j = 1; j <= w2len; j++){
+                a[i][j] = min(
+                    a[i-1][j-1] + (w1.charAt(i-1) == w2.charAt(j-1) ? 0 : 1),
+                    a[i][j-1] + 1,
+                    a[i-1][j] + 1
+                    );
+            }
+        }
+        for(int i = w1len; i >= 0; i--){
+            for(int j = 0; j <= w2len; j++){
+                int res = a[i][j];
                 System.out.print(res + ",");
             }
             System.out.print('\n');
         }
-        //partDist(args[0], args[1], Integer.parseInt(args[2]), Integer.parseInt(args[2]));
-        //System.out.println(count);
-
-        
+        return a[w1len-1][w2len-1];
     }
-
+    public static void main(String[] args) {
+        
+        // for(int i = Integer.parseInt(args[3]); i >= 0; i--){
+        //     for(int j = 0; j <= Integer.parseInt(args[2]); j++){
+        //         int res = partDist(args[0],args[1],j,i);
+        //         System.out.print(res + ",");
+        //     }
+        //     System.out.print('\n');
+        // }
+        double start = System.currentTimeMillis();
+        partDist(args[0], args[1], Integer.parseInt(args[2]), Integer.parseInt(args[2]));
+        double totalTime = System.currentTimeMillis() - start;
+        System.out.println("Time spent: " + totalTime + "ms");
+        //System.out.println(count);
+    }
 }
