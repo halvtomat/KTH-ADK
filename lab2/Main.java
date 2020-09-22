241f61aa8dde9b9e5dd0c9db8446b290178eb0d4
Daniel <danne_1998@hotmail.se>
2020-09-22 21:18:26 +0200

    finished lab2 minor changes to masterPractice1

--- lab2/Main.java
+++ lab2/Main.java
@@ -21,14 +21,22 @@ public class Main {
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
         List<String> wordList = readWordList(stdin);
         String word;
+        int a[][] = new int[40][40];
+        for(int i = 0; i < 40; i++){
+            a[i][0] = i;
+            a[0][i] = i;
+        }
+        // double start2 = System.currentTimeMillis();
         while ((word = stdin.readLine()) != null) {
-            ClosestWordsOptimized closestWords = new ClosestWordsOptimized(word, wordList);
+            ClosestWordsOptimized closestWords = new ClosestWordsOptimized(word, wordList, a);
             System.out.print(word + " (" + closestWords.getMinDistance() + ")");
             for (String w : closestWords.getClosestWords())
                 System.out.print(" " + w);
             System.out.println();
         }
+        // double ClosestWordsTime = System.currentTimeMillis() - start2;
         // double totalTime = System.currentTimeMillis() - start;
+        // System.out.println("ClosestWordsTime = " + ClosestWordsTime);
         // System.out.println("totaltime = " + totalTime);
     }
 }
@@ -37,38 +45,30 @@ class ClosestWordsOptimized {
     LinkedList<String> closestWords = null;
 
     int closestDistance = -1;
+    String lw = "";
 
     int min(int a, int b, int c){
         return Math.min(a, Math.min(b,c));
     }
 
-    int partDist(String w1, String w2, int w1len, int w2len) {
-        int a[][] = new int[w1len+1][w2len+1];
-        for(int i = 0; i <= w1len; i++)
-            a[i][0] = i;
-        for(int i = 1; i <= w2len; i++)
-            a[0][i] = i;
-        for(int i = 1; i <= w1len; i++){
-            for(int j = 1; j <= w2len; j++){
-                int sub = (w1.charAt(i-1) == w2.charAt(j-1) ? 0 : 1);
-                a[i][j] = min(
-                    a[i-1][j-1] + sub,
-                    a[i][j-1] + 1,
-                    a[i-1][j] + 1
-                    );
+    int partDist(String w1, String w2, int w1len, int w2len, int a[][]) {
+        int match = matching(w2, lw);
+        for(int j = match + 1; j <= w2len; j++){
+            for(int i = 1; i <= w1len; i++){
+                int sub = a[i-1][j-1] + (w1.charAt(i-1) == w2.charAt(j-1) ? 0 : 1);
+                int del = a[i][j-1] + 1;
+                int add = a[i-1][j] + 1;
+                a[i][j] = min(sub, del, add);
             }
         }
+        lw = w2;
         return a[w1len][w2len];
     }
 
-    int distance(String w1, String w2) {
-        return partDist(w1, w2, w1.length(), w2.length());
-    }
-
-    public ClosestWordsOptimized(String w, List<String> wordList) {
+    public ClosestWordsOptimized(String w, List<String> wordList, int a[][]) {
         for (String s : wordList) {
             if(Math.abs(w.length() - s.length()) > closestDistance && closestDistance != -1) continue;
-            int dist = distance(w, s);
+            int dist = partDist(w, s, w.length(), s.length(), a);
             if (dist < closestDistance || closestDistance == -1) {
                 closestDistance = dist;
                 closestWords = new LinkedList<String>();
@@ -77,6 +77,13 @@ class ClosestWordsOptimized {
             else if (dist == closestDistance)
                 closestWords.add(s);
         }
+    }
+
+    int matching(String s, String w){
+        int len = Math.min(s.length(), w.length());
+        for(int i = 0; i < len; i++)
+            if(s.charAt(i) != w.charAt(i)) return i;
+        return 0;
     }
 
     int getMinDistance() {
