4d323642450330fb13a99f46a130c175447cf1c5
Daniel <danne_1998@hotmail.se>
2020-10-13 10:54:15 +0200

    progress in step1 of lab3

--- lab3/step1.cpp
+++ lab3/step1.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-int vertix_amount(){
-    int x,y;
-    std::cin >> x;
-    std::cin >> y;
-    return x + y;
-}
+using std::cin;
+using std::cout;
+using std::vector;
+using std::find;
 
-void add_edge(std::vector<int> adj[], int u, int v){
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+void reduce_to_flow(){
+    int x, y, e, k, u;
+    cin >> x >> y >> e;
+    k = 1;
+    u = x + y + 2;
+    cout << u << "\n" << k << " " << u << "\n" << x +y+2 << "\n";
+
+    vector<int> x_vec;
+    vector<int> y_vec;
+    for(int i = 0; i < e; i++){
+        int a, b;
+        cin >> a >> b;
+        cout << a+1 << " " << b+1 << " " << k << "\n";
+        if(find(x_vec.begin(), x_vec.end(), a) == x_vec.end()) x_vec.push_back(a);
+        if(find(y_vec.begin(), y_vec.end(), a) == y_vec.end()) y_vec.push_back(b);
+    }
+    while(!x_vec.empty()){
+        int a = x_vec.back();
+        x_vec.pop_back();
+        cout << k << " " << a+1  << " " << 1 << "\n";
+    }
+    while(!y_vec.empty()){
+        int b = y_vec.back();
+        y_vec.pop_back();
+        cout << b+1 << " " << u << " " << 1 << "\n";
+    }
+    cout.flush();
 }
 
 int main(int argc, char const *argv[]){
-    int v_size = vertix_amount();
-    std::vector<int> adj[v_size];
-    int n = 0;
-    std::cin >> n;
-    for (int i = 0; i < n; i++){
-        int v, u;
-        std::cin >> v;
-        std::cin >> u;
-        add_edge(adj,u,v);
-    }
-
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    reduce_to_flow();
     return 0;
 }
