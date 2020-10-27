e34e17489de764e91040408c5e9c8ae48cd1c969
Daniel <danne_1998@hotmail.se>
2020-10-27 13:27:03 +0100

    added makefile and updated test.sh

--- lab3/test.sh
+++ lab3/test.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-DIFF=$(cmp <(head ut1 -n 3) <(head ut2 -n 3))
-COUNT=0
+make clean
+make step2
 
 while [ "$?" -eq 0 ]
 do
-echo $DIFF
     ./flowgen 6 18 10 > test_auto
     sleep 0.001
-    DIFF=$(cmp <(head <(./maxflow < test_auto) -n 3) <(head <(./a.out < test_auto) -n 3))
+    DIFF=$(cmp <(head <(./maxflow < test_auto) -n 3) <(head <(./step2 < test_auto) -n 3))
 done
+clear
+echo ------TEST_AUTO----
+cat test_auto
+echo ------MAXFLOW------
+./maxflow < test_auto
+echo ------STEP2--------
+./step2 < test_auto
