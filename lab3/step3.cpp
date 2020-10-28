1fb63da0ff35f0e2350a0cc11630eb1a2fb1be95
Daniel <danne_1998@hotmail.se>
2020-10-28 15:20:11 +0100

    finished lab3 completely

--- lab3/step3.cpp
+++ lab3/step3.cpp
@@ -7,72 +7,59 @@
 using std::cin;
 using std::cout;
 using std::vector;
-using std::find;
 using std::queue;
 using std::pair;
+using std::find;
 using std::remove;
 using std::make_pair;
 using std::min;
 
-int x, y, k, u, n, f;
-int cap[10000][10000] = {};
-int real_flow[10000][10000] = {};
-vector<vector<int>> adj;
-vector<pair<int,int>> real_edge;
+int16_t x, y, k, u, n, f;
+bool cap[10002][10002] = {};
+bool real_flow[10002][10002] = {};
+vector<vector<int16_t>> adj;
+vector<pair<int16_t,int16_t>> real_edge;
 
-int bfs(vector<int>& path){
+bool bfs(vector<int16_t>& path){
     fill(path.begin(), path.end(), -1);
     path[k] = -2;
-    queue<pair<int,int>> q;
-    q.push({k, 200000000});
+    queue<int16_t> q;
+    q.push(k);
     while(!q.empty()) {
-        int current = q.front().first;
-        int flow = q.front().second;
+        int16_t current = q.front();
         q.pop();
-        for(int next : adj[current]){
-            if(path[next] == -1 && cap[current][next] > 0) {
+        for(int16_t next : adj[current]){
+            if(path[next] == -1 && cap[current][next]) {
                 path[next] = current;
-                int new_flow = min(flow, cap[current][next]);
-                
-                if(next == u) return new_flow;
-                q.push({next, new_flow});
+                if(next == u) return true;
+                q.push(next);
             }
         }
     }
-    return 0;
+    return false;
 }
 
 void max_flow(){
-    int flow = 0;
-    vector<int> path(n);
-    int new_flow;
+    int16_t flow = 0;
+    vector<int16_t> path(n);
 
-    while(new_flow = bfs(path)){
-        flow += new_flow;
-        int current = u;
+    while(bfs(path)){
+        flow++;;
+        int16_t current = u;
 
         while(current != k){
-            int prev = path[current];
-            cap[prev][current] -= new_flow;
-            cap[current][prev] += new_flow;
+            int16_t prev = path[current];
+            cap[prev][current] = false;
+            cap[current][prev] = true;
 
-            if(real_flow[current][prev] == 0){
-                if(real_flow[prev][current] == 0)
-                    real_edge.push_back({prev, current});
-                real_flow[prev][current] += new_flow;
+            if(!real_flow[current][prev]){
+                real_edge.push_back({prev, current});
+                real_flow[prev][current] = true;
             }
             else{
-                if(real_flow[current][prev] > new_flow)
-                    real_flow[current][prev] -= new_flow;
-                else{
-                    auto p = make_pair(current, prev);
-                    real_edge.erase(remove(real_edge.begin(), real_edge.end(), p), real_edge.end());
-                    if(real_flow[current][prev] < new_flow){
-                        real_edge.push_back({prev, current});
-                        real_flow[prev][current] = new_flow;
-                    }
-                    real_flow[current][prev] = 0;
-                }
+                auto p = make_pair(current, prev);
+                real_edge.erase(remove(real_edge.begin(), real_edge.end(), p), real_edge.end());
+                real_flow[current][prev] = false;
             }
             current = prev;
         }
@@ -80,7 +67,7 @@ void max_flow(){
     f = flow;
 }
 
-void add_edge(int a, int b, int c){
+void add_edge(int16_t a, int16_t b, bool c){
     if(find(adj[a].begin(), adj[a].end(), b) != adj[a].end()){
         cap[a][b] = c;
     }
@@ -88,7 +75,7 @@ void add_edge(int a, int b, int c){
         adj[a].push_back(b);
         adj[b].push_back(a);
         cap[a][b] = c;
-        cap[b][a] = 0;
+        cap[b][a] = false;
     }
 }
 
@@ -102,22 +89,21 @@ void reduce_to_flow(){
     adj.resize(n);
 
     for(int i = 0; i < e; i++){
-        int a, b;
+        int16_t a, b;
         cin >> a >> b;
-        add_edge(a,b,1);
+        add_edge(a,b,true);
     }
     for(int i = 1; i < x+1; i++)
-        add_edge(k,i,1);
+        add_edge(k,i,true);
     for(int i = 1; i < y+1; i++)
-        add_edge(x+i,u,1);
+        add_edge(x+i,u,true);
 }
 
 void flow_to_match() {
     cout << x << " " << y << "\n" << f << "\n";
-
     for (int i = 0; i < real_edge.size(); i++) {
-        int a = real_edge[i].first;
-        int b = real_edge[i].second;
+        int16_t a = real_edge[i].first;
+        int16_t b = real_edge[i].second;
         if(a != k && b != u) cout << a << " " << b << "\n";
     }
     cout.flush();
