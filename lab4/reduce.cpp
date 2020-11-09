cc98db0409ad0da7d7afa041bdd81b49d84b0020
Daniel <danne_1998@hotmail.se>
2020-11-09 13:08:34 +0100

    fixed some bugs

--- lab4/reduce.cpp
+++ lab4/reduce.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using std::cin;
 using std::cout;
+using std::vector;
+using std::string;
+using std::to_string;
 
 int main(int argc, char const *argv[]){
     int v,e,c;
     cin >> v >> e >> c;
+    vector<bool> role(v);
+    string s;
     if(v < c) c = v;
-    cout << v+3 << "\n" << e+2 << "\n" << c+3 << "\n";
 
-    cout << "1 1\n1 2\n1 3\n"; 
+    s.append("1 1\n1 2\n");
     for(int i = 0; i < v; i++){
-        cout << c;
+        role[i] = false;
+        s.append(to_string(c));
         for(int j = 0; j < c; j++)
-            cout << " " << j+4;
-        cout << "\n";
+            s.append(" " + to_string(j+3));
+        s.append("\n");
     }
 
-    cout << "2 1 3\n2 2 3\n";
+    s.append("2 1 3\n2 2 3\n");
+    role[0] = true;
     for(int i = 0; i < e; i++){
         int a,b;
         cin >> a >> b;
-        cout << 2 << " " << a+3 << " " << b+3 << "\n";
+        role[a] = true;
+        role[b] = true;
+        s.append("2 " + to_string(a+2) + " " + to_string(b+2) + "\n");
     }
+
+    for(int i = 0; i < v; i++){
+        if(role[i] == false){
+            e++;
+            s.append("2 1 " + to_string(i+3) + "\n");
+        }
+    }
+
+    cout << v+2 << "\n" << e+2 << "\n" << c+2 << "\n";
+    cout << s;
+    
     return 0;
 }
